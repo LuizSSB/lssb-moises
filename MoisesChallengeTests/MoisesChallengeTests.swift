f394910ec2c05ab0b9b151ed3471ff271abe4d73
Luiz <luizssb.biz@gmail.com>
2026-03-31 13:32:48 -0300

    Initial Commit

---
+++ MoisesChallengeTests/MoisesChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoisesChallengeTests.swift
+//  MoisesChallengeTests
+//
+//  Created by Luiz SSB on 31/03/26.
+//
+
+import Testing
+@testable import MoisesChallenge
+
+struct MoisesChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
